3309380bdf7880964365d3f3e2f4ce37e0ab4cca
Tyler Snow <ts6886@hound.cs.mun.ca>
2018-11-26 13:37:55 -0330

    Added UI and SVM algorithm

---
+++ src/SVM.py
@@ -0,0 +1,55 @@
+import sklearn
+import os
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn import datasets
+from sklearn import cross_validation
+from sklearn.model_selection import cross_val_score;
+import pprint as pp
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy as sp
+from sklearn.decomposition import PCA
+from sklearn.pipeline import Pipeline
+
+def trainSVM(path):
+    files=sklearn.datasets.load_files(path, shuffle=True)
+    #print(files)
+    model=CountVectorizer(analyzer='word',stop_words=["the","is","an","and"])
+    print(model)
+    X_train=model.fit_transform(files.data)
+    #print(X_train.toarray())
+    tf_transformer = TfidfTransformer()
+    #X = tf_transformer.fit_transform(X_train)
+    pipeline=Pipeline([('vect',CountVectorizer()),('tfidf',TfidfTransformer()),])
+    print(pipeline)
+    X = pipeline.fit_transform(files.data).todense()
+    pca=PCA(n_components=2).fit(X)
+    data=pca.transform(X)
+    #plots input data
+    plt.scatter(data[:,0],data[:,1],c=files.target)
+    plt.show()
+    clf = sklearn.svm.LinearSVC()
+    test_classifier(X, files.target, clf, test_size=0.2, y_names=files.target_names, confusion=False)
+
+def test_classifier(X, y, clf, test_size=0.3, y_names=None, confusion=False):
+    #train-test split
+    X_train, X_test, y_train, y_test = sklearn.cross_validation.train_test_split(X, y, test_size=test_size)
+    clf.fit(X_train, y_train)
+    y_predicted = clf.predict(X_test)
+    #print(X_train[:,0].todense(),X_train[:,1].todense())
+    #plt.scatter(X_test[:,0].todense(),X_test[:,1].todense(),c='b')
+    print(y_test)
+    print(y_predicted)
+    #plt.scatter(y_predicted/X_test,c='r')
+    plt.show()
+
+def main():
+    root_path=os.path.dirname(os.path.abspath(__file__))
+    parent_path=os.path.dirname(os.path.abspath(root_path))
+    pos_dir=os.path.join(parent_path,"Datasets/testSets")
+    trainSVM(pos_dir)
+
+if __name__ == "__main__":
+    main()
---
+++ src/sentimentAnalysisGUI.py
@@ -0,0 +1,67 @@
+#from Tkinter import Tk, Label, Button, Listbox
+from Tkinter import *
+class sentimentAnalysisGUI:
+    
+    def callSelectionMethods(self):
+        if(len(self.algorithms.curselection())!=0 and len(self.testSet.curselection())!=0):
+            self.selectAlg()
+            self.selectTest()
+        else:
+            print("Please select an algorithm and a test set.")
+    def selectAlg(self):
+        clist = list()
+        selection = self.algorithms.curselection()
+        for i in selection:
+            choice= self.algorithms.get(i)
+            clist.append(choice)
+        for val in clist:
+            print(val)
+
+    def selectTest(self):
+        tlist = list()
+        selection = self.testSet.curselection()
+        for i in selection:
+            choice= self.testSet.get(i)
+            tlist.append(choice)
+        for val in tlist:
+            print(val)
+            
+    def __init__(self, master):
+        self.master = master
+        master.title("Sentiment Analysis")
+        master.geometry("800x500")
+        master.option_add("*Button.Background", "white")
+        master.option_add("*Button.Foreground", "red")
+        master.option_add("*Label.Background","white")
+        master.option_add("*Label.Foreground","red")
+        master.configure(background='white')
+        
+        self.algLabel = Label(master, text="Algorithm")
+        self.algLabel.pack()
+
+        self.algorithms = Listbox(master,selectmode=SINGLE,height=2,exportselection=0)
+        self.algorithms.insert(1,"Naive Bayes")
+        self.algorithms.insert(2,"Support Vector Machine")
+        alg=self.algorithms.curselection()
+        self.algorithms.pack()
+
+        self.testLabel = Label(master, text="Test Set")
+        self.testLabel.pack()
+        self.testSet = Listbox(master,selectmode=SINGLE,height=1,exportselection=0)
+        self.testSet.insert(1,"IMDB Movie test set")
+        self.testSet.pack()
+        
+        self.test_button = Button(master, text="Test", command=self.callSelectionMethods)
+        self.test_button.pack()
+
+        self.close_button = Button(master, text="Close", command=master.quit)
+        self.close_button.pack()
+
+
+
+root = Tk()
+gui = sentimentAnalysisGUI(root)
+root.mainloop()
+
+#win=Tkinter.Tk()
+#win.mainloop()
