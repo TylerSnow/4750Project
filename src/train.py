80f907ff7c6b228c561f7ce971f7a0292d335253
Eric <EricRoy.Elli@gmail.com>
2018-11-26 17:15:37 -0330

    Fixed linted errors

--- src/train.py
+++ src/train.py
@@ -1,26 +1,23 @@
 import tokenize
 import os
-import sys
 import naiveBayes
 
 
-"""
-This function gets the full paths for each positive and negative review
-
-Args:
-    None
-
-Returns:
-    neg_paths - the full path to each negative review
-    pos_paths - the full path to each positive review
-"""
+# This function gets the full paths for each positive and negative review
+#
+# Args:
+#     None
+#
+# Returns:
+#     neg_paths - the full path to each negative review
+#     pos_paths - the full path to each positive review
 def get_training_filepaths():
     neg_paths = []
     pos_paths = []
     root_dir = os.path.dirname(os.path.abspath(__file__))
     parent_dir = os.path.dirname(os.path.abspath(root_dir))
-    neg_dir = os.path.join(parent_dir,"Datasets/aclImdb/train/neg/")
-    pos_dir = os.path.join(parent_dir,"Datasets/aclImdb/train/pos/")
+    neg_dir = os.path.join(parent_dir, "Datasets/aclImdb/train/neg/")
+    pos_dir = os.path.join(parent_dir, "Datasets/aclImdb/train/pos/")
 
     for dirName, subdirList, fileList in os.walk(neg_dir):
         for fname in fileList:
@@ -33,18 +30,16 @@ def get_training_filepaths():
     return neg_paths, pos_paths
 
 
-"""
-This function tokenizes negative and positive files and returns the tokenized
-review with a "pos" or "neg" marker
-
-Args:
-    neg_fnames - the paths of the negative reviews
-    pos_fnames - the paths of the positive reviews
-
-Returns:
-    neg_reviews - the tokenized and marked negative reviews
-    pos_reviews - the tokenized and marked positive reviews
-"""
+# This function tokenizes negative and positive files and returns the tokenized
+# review with a "pos" or "neg" marker
+#
+# Args:
+#     neg_fnames - the paths of the negative reviews
+#     pos_fnames - the paths of the positive reviews
+#
+# Returns:
+#     neg_reviews - the tokenized and marked negative reviews
+#     pos_reviews - the tokenized and marked positive reviews
 def process_files(neg_fnames, pos_fnames):
     neg_reviews = []
     for file in neg_fnames:
@@ -62,7 +57,7 @@ def process_files(neg_fnames, pos_fnames):
 
 
 def main():
-    #Retrieving and processing the training data
+    # Retrieving and processing the training data
     neg_fnames, pos_fnames = get_training_filepaths()
     neg_reviews, pos_reviews = process_files(neg_fnames, pos_fnames)
     print("done processing files")
@@ -70,7 +65,6 @@ def main():
     reviews = [neg_reviews, pos_reviews]
 
     naiveBayes.naiveBayes(reviews)
-
 
 
 if __name__ == '__main__':
